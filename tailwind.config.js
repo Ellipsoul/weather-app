87e53a0da21005ecae26a31078a07827fc6ee9bf
Aron Teh <aronteh.chess@gmail.com>
2022-05-11 16:24:35 +0100

    Configured TailwindCSS, changing from SASS to CSS

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  mode: "jit",
+  important: true, // Might be necessary to overwrite material design defaults
+  content: ["./src/**/*.{html,ts}"],
+  darkMode: "class",
+  theme: {
+    container: {
+      padding: "2rem",
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ['"Fira Code"', "serif"],
+    },
+    extend: {
+      screens: {
+        mobile: "425px",
+      },
+    },
+  },
+  variants: {},
+};
