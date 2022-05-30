d5c7afdb699d787f0327ff0bcfadde556dec4fda
Aron Teh <aronteh.chess@gmail.com>
2022-05-30 18:08:35 +0100

    Made some progress on the top bar. Currently buttons are not working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule, Title } from '@angular/platform-browser';
 import { AppRoutingModule } from './routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { environment } from '../environments/environment';
 
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
-import { environment } from '../environments/environment';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { HomepageComponent } from './components/homepage/homepage.component';
-import { ProfileComponent } from './components/profile/profile.component';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -24,6 +26,7 @@ import { HashLocationStrategy, LocationStrategy } from '@angular/common';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatButtonModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the application is stable
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,21 @@
 module.exports = {
-  mode: "jit",
+  mode: 'jit',
   important: true, // Might be necessary to overwrite material design defaults
-  content: ["./src/**/*.{html,ts}"],
-  darkMode: "class",
+  content: ['./src/**/*.{html,ts}'],
+  darkMode: 'class',
   theme: {
     container: {
-      padding: "2rem",
+      padding: '2rem',
     },
     fontFamily: {
-      sans: ["Graphik", "sans-serif"],
-      serif: ['"Fira Code"', "serif"],
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['"Fira Code"', 'serif'],
+      title: ['Changa', 'sans-serif'],
+      slogan: ['Chivo', 'sans-serif'],
     },
     extend: {
       screens: {
-        mobile: "425px",
+        mobile: '425px',
       },
     },
   },
