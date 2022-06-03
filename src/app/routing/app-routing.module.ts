42ab6f13d13a9567f43c82a12b5ca0c8b4a5377a
Aron Teh <aronteh.chess@gmail.com>
2022-06-03 16:56:49 +0100

    Added auth guard for profile page

--- src/app/routing/app-routing.module.ts
+++ src/app/routing/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '@angular/fire/auth-guard';
 
 import { ProfileComponent } from '../components/profile/profile.component';
 import { HomepageComponent } from '../components/homepage/homepage.component';
 
 const routes: Routes = [
   { path: 'app', component: HomepageComponent, data: { title: 'SkyStory - App' } },
-  { path: 'profile', component: ProfileComponent, data: { title: 'SkyStory - Profile' } },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    data: { title: 'SkyStory - Profile' },
+    canActivate: [AuthGuard],
+  },
   { path: '**', redirectTo: '/app'},
 ];
 
