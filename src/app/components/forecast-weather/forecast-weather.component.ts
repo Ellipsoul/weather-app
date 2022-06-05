d3a5bb8e2314bdd27cb82618f3bc61923d60cded
Aron Teh <aronteh.chess@gmail.com>
2022-06-05 23:37:50 +0100

    Forecast weather component set up well for success tomorrow

--- src/app/components/forecast-weather/forecast-weather.component.ts
+++ src/app/components/forecast-weather/forecast-weather.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
-import { WeatherForecastResponse } from 'src/app/services/weatherapi.service';
-import { WeatherdataService } from 'src/app/services/weatherdata.service';
+import { ForecastDayObject, WeatherForecastResponse } from 'src/app/services/weatherapi.service';
+import { dayWeatherTypeMap, nightWeatherTypeMap, UnitSystem,
+  WeatherdataService } from 'src/app/services/weatherdata.service';
 
 @Component({
   selector: 'app-forecast-weather',
@@ -12,15 +13,46 @@ export class ForecastWeatherComponent implements OnDestroy {
   // Forecast Weather data retrieved from service
   forecastWeatherData: WeatherForecastResponse | undefined;
   forecastWeatherDataSubscription: Subscription;
+  // Useful variables extracted from forecastWeatherData
+  // temperature: string | undefined;
+  locationString: string | undefined;
+  weatherBackgrounds: string[] | undefined;
+  // Keep track of metric or imperial units to display
+  unitSystem: UnitSystem;
 
   constructor(private weatherdataService: WeatherdataService) {
+    // Subscribe to the incoming forecast weather data
     this.forecastWeatherDataSubscription = this.weatherdataService.forecastWeatherData.subscribe(
         (weatherForecastResponse: WeatherForecastResponse) => {
           this.forecastWeatherData = weatherForecastResponse;
+          this.updateUsefulVariables();
         });
+    // Retrieve the preferred unit system, defaulting to metric
+    const unitSystem = localStorage.getItem('unitSystem');
+    if (unitSystem === null) {
+      localStorage.setItem('unitSystem', UnitSystem.Metric);
+    }
+    this.unitSystem = localStorage.getItem('unitSystem') as UnitSystem;
+  }
+
+  // Switch between metric and imperial units
+  toggleUnitSystem($event: string): void {
+    this.unitSystem = $event as UnitSystem;
+    localStorage.setItem('unitSystem', $event);
+    this.updateUsefulVariables();
   }
 
   ngOnDestroy(): void {
     this.forecastWeatherDataSubscription.unsubscribe();
+  }
+
+  private updateUsefulVariables(): void {
+    this.locationString =
+      `${this.forecastWeatherData!.location.name}, ${this.forecastWeatherData!.location.country}`;
+    const weatherCodes: string[] = this.forecastWeatherData!.forecast.forecastday.map(
+        (forecastDay: ForecastDayObject) => forecastDay.day.condition.code.toString());
+    this.weatherBackgrounds = this.forecastWeatherData?.current.is_day === 1 ?
+      weatherCodes.map((weatherCode: string) => dayWeatherTypeMap[weatherCode]) :
+      weatherCodes.map((weatherCode: string) => nightWeatherTypeMap[weatherCode]);
   }
 }
